f5432a1c3f352973f6ff0e594356e45580bbe0c7
SaidCarrera <145997306+SaidCarrera@users.noreply.github.com>
2025-06-24 02:09:15 +0000

    Agrega ejecución de checkstyle

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -6,6 +6,8 @@ public class Main {
         try (PrintWriter w = new PrintWriter("output/index.html", "UTF-8")) {
             w.println("<h1>Hello from Java CI/CD!</h1>");
             w.println("<h1>Esto es un despliegue continuo!</h1>");
+            w.println("<h1>Hello from Java CI/CD!</h1>");
+            w.println("<p><a href=\"checkstyle.html\">Ver reporte Checkstyle</a></p>");
             System.out.println("HTML generado correctamente.");
         } catch (IOException e) {
             e.printStackTrace();
